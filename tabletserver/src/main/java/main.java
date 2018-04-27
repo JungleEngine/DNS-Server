66abba78804d26d9b37b7cd82a801531ef90003f
AhmedSa-mir <asamir97@hotmail.com>
2018-04-27 02:42:41 +0200

    -

--- tabletserver/src/main/java/main.java
+++ tabletserver/src/main/java/main.java
@@ -38,22 +38,8 @@ public class main {
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 
-            // Consume the initial welcoming messages from the server
-            System.out.println(in.readLine());
-
-            BufferedReader consolereader = new BufferedReader (new InputStreamReader(System.in));
-            String message = "";
-            while(true)
-            {
-                System.out.println("Enter a message in lower case or a period '.' to quit");
-                message = consolereader.readLine();
-                out.println(message); //send message to server
-                if(message.equals("."))
-                    break;
-                String response = in.readLine();
-                System.out.println("Capitalized message = "+ response + "\n");
-            }
             socket.close();
+
         }catch (Exception e)
         {
             e.printStackTrace();
